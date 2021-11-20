91a423b19e24640f8d4026927c31c6d31f7db3e4
JhonnyMacarony <91407912+JhonnyMacarony@users.noreply.github.com>
2021-11-20 12:39:47 +0200

    first commit

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+require('dotenv').load()
 }
 
-const express = require("express")
+const express = require('express')
 const app = express()
-const expressLayouts = require("express-ejs-layouts")
+const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
-app.set("view engine", "ejs")
-app.set("views", __dirname + '/views')
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
-app.set('layout', 'layouts/project1')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
-db.once('open', ()=> console.log('Connected to Mongoose'))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
-console.log('The server is running on port 3000')
